5e8cf2a48bbb97d90c209d37a07015d714ca88b8
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-04 16:26:34 -0700

    completed first challenge

--- src/com/jjkeeney/Main.java
+++ src/com/jjkeeney/Main.java
@@ -3,6 +3,10 @@ package com.jjkeeney;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int myValue = 10000;
+        byte myByte = 10;
+        short myShort = 100;
+        long myLong = (long) 50000 + (10* (myShort+myByte+myValue));
+        System.out.println(myLong);
     }
 }
